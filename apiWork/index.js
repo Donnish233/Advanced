6f7260e9096b779b185d8b7c7b4fcdaea2026a91
Michael Okoronkwo <okoronkwomikec@gmail.com>
2025-08-05 19:22:52 +0100

    Api Structure pratice

---
+++ apiWork/index.js
@@ -0,0 +1,24 @@
+let userList = document.getElementById("user-list");
+let errMsg = document.getElementById("errMsg");
+
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+function getUsers() {
+    fetch(API_URL)
+        .then((response) => response.json())
+        .then((data) => {
+            data.forEach((user) => {
+                let userDiv = document.createElement("div");
+                userDiv.classList.add("user");
+                userDiv.innerHTML = `
+                <h3>${user.name}</h3>
+                <p>${user.email}</p>
+                `;
+                userList.appendChild(userDiv);
+            });
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+            errMsg.style.display("block");
+        });
+}
